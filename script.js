39ffcc4ca634546c345ebd6e610bc8df4b506dd4
ThibautJanssens <janssensthib@gmail.com>
2019-02-11 14:53:46 +0100

    adding async funct for the cycler

--- script.js
+++ script.js
@@ -6,8 +6,17 @@ import 'bootstrap';
 
 // import the style
 import "./style.scss";
+import Axios from "axios";
 
 /*
   Put the JavaScript code you want below.
 */
 
+setInterval(()=>{
+  getCat();
+}, 5000);
+
+async function getCat(){
+  let cat = await Axios.get("https://aws.random.cat/meow");
+  document.querySelector('#imageHolder').innerHTML = "<img id='cats' src="+cat.data.file+">";
+}
